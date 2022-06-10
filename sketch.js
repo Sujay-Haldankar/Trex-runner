b5b92be14e03143a8004b9ff5a08f9383c0d9ee7
Sujay-Haldankar <107251249+Sujay-Haldankar@users.noreply.github.com>
2022-06-10 16:14:36 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -42,6 +42,9 @@ function setup() {
   trex.addAnimation("collided", trex_collided);
   trex.scale = 0.5;
   
+  trex.setCollider("circle",0,0,40);
+  trex.debug = true
+  
   ground = createSprite(200,180,400,20);
   ground.addImage("ground",groundImage);
   ground.x = ground.width /2;
